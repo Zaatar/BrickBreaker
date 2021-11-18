b61b9290004b77d42e6b9f4f0a3649e6a1fc0372
Zaatar <hani.zaatar.92@gmail.com>
2021-11-18 18:34:44 +0100

    Adding the Shader classes and the Square classes

---
+++ src/engine/Square.cpp
@@ -0,0 +1,18 @@
+#include "Square.h"
+
+void Square::load()
+{
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+}
+
+void Square::render()
+{
+    //To be added after Shader is added
+}
---
+++ src/engine/Square.h
@@ -0,0 +1,38 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+#include <GL/glew.h>
+#include <iostream>
+
+class Square
+{
+public:
+    Square() = default;
+    ~Square() = default;
+
+    void load();
+    void render();
+
+private:
+    GLuint vao;
+    GLuint vbo;
+    float lastPositionX = 0.5f;
+    float lastPositionY = 0.5f;
+    float speedX = 1.0f;
+    float speedY = 1.0f;
+
+    float scaleMatrix[16] = {};
+
+    float points[16] = {
+        -0.5, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f};
+
+    float translationMatrix[16] = {
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f};
+};
+#endif
